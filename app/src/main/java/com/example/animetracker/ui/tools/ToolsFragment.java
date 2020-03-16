17adcdd3940d774cc301df2ce02fde13482dd81f
Martin Nguyen <martinnguyen0523@gmail.com>
2020-03-16 15:31:39 -0700

    added functions for file writing

--- app/src/main/java/com/example/animetracker/ui/tools/ToolsFragment.java
+++ app/src/main/java/com/example/animetracker/ui/tools/ToolsFragment.java
@@ -1,35 +1,85 @@
 package com.example.animetracker.ui.tools;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.animetracker.AnimeListViewModel;
 import com.example.animetracker.R;
+import com.example.animetracker.data.AnimeDatabaseEntry;
+import com.example.animetracker.utils.FileExportUtils;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class ToolsFragment extends Fragment {
 
     private ToolsViewModel toolsViewModel;
+    private AnimeListViewModel animeListViewModel;
+    private List<AnimeDatabaseEntry> animeDatabaseEntries;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        toolsViewModel =
-                ViewModelProviders.of(this).get(ToolsViewModel.class);
+//        toolsViewModel =
+//                ViewModelProviders.of(this).get(ToolsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tools, container, false);
-        final TextView textView = root.findViewById(R.id.text_tools);
-        toolsViewModel.getText().observe(this, new Observer<String>() {
+//        final TextView textView = root.findViewById(R.id.text_tools);
+//        toolsViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        animeListViewModel = new ViewModelProvider(
+                this,
+                new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication())
+        ).get(AnimeListViewModel.class);
+
+        Button exportButton = root.findViewById(R.id.btn_export_anime_list);
+        exportButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v){
+                Context context = getActivity().getApplicationContext();
+                String json = generateJson(animeDatabaseEntries);
+                writeToFile(json, context);
             }
         });
+
         return root;
+    }
+
+    public String generateJson (List<AnimeDatabaseEntry> animeDatabaseEntries) {
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<AnimeDatabaseEntry>>(){}.getType();
+        String json = gson.toJson(animeDatabaseEntries, type);
+        return json;
+    }
+
+    public void writeToFile (String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("AnimeListExport.json", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
     }
 }
